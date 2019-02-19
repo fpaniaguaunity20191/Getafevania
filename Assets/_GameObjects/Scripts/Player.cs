116d9429cfd3eeaf45c32745a0051541f9a237f7
Fernando Paniagua <fpaniagua.unity.2018.9@gmail.com>
2019-02-19 14:11:03 +0100

    Initial commit

---
+++ Assets/_GameObjects/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    [SerializeField] float linearSpeed;//Velocidad horizontal
+    private Rigidbody2D rb2d;
+    private float x, y;
+    private void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        x = Input.GetAxis("Horizontal");
+        y = Input.GetAxis("Vertical");
+    }
+    private void FixedUpdate()
+    {
+        Walk();        
+    }
+    private void Walk()
+    {
+        if (Mathf.Abs(x) > 0) {
+            rb2d.velocity = new Vector2(x * linearSpeed, 0);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        print("ORANGE");
+    }
+}
